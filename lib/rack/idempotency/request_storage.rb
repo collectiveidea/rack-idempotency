ccc65e53839cb73394150ffa4102bfd80f568b17
Daniel Morrison <daniel@collectiveidea.com>
2022-11-26 10:46:03 -0500

    Remove SAFE_METHODS
    We will use the presence of the idempotency key header alone to determine cachability.

--- lib/rack/idempotency/request_storage.rb
+++ lib/rack/idempotency/request_storage.rb
@@ -5,22 +5,20 @@ require 'openssl/hmac'
 module Rack
   class Idempotency
     class RequestStorage
-      SAFE_METHODS = %w[GET HEAD OPTIONS TRACE PUT DELETE]
-
       def initialize(store, request)
         @store   = store
         @request = request
       end
 
       def read
-        return if SAFE_METHODS.include?(request.request_method) || request.idempotency_key.nil?
+        return if request.idempotency_key.nil?
 
         stored = store.read(key)
         JSON.parse(stored) if stored
       end
 
       def write(response)
-        return if SAFE_METHODS.include?(request.request_method) || request.idempotency_key.nil?
+        return if request.idempotency_key.nil?
 
         store.write(key, response.to_json)
       end
